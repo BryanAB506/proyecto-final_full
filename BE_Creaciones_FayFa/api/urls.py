91664c0de0d1708b4f314addc2c7566d573f3691
BryanAB506 <baragon@fwdcostarica.com>
2024-11-19 14:05:28 -0600

    cambios de back, token

--- BE_Creaciones_FayFa/api/urls.py
+++ BE_Creaciones_FayFa/api/urls.py
@@ -1,28 +1,37 @@
-from django.urls import path, include
-from django.contrib import admin
+from django.urls import path
 from . import views
-from .views import RegistroView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-
 urlpatterns = [
-    
-    path('Usuarios/', views.UsuariosListCreate.as_view(), name='Usuarios-list'),
-    path('Usuarios/<int:pk>/', views.UsuariosDetail.as_view(), name='Usuarios-detail'),
+
+
+    # Direcciones de envío
     path('Direcciones_envio/', views.Direcciones_envioListCreate.as_view(), name='Direcciones_envio-list'),
     path('Direcciones_envio/<int:pk>/', views.Direcciones_envioDetail.as_view(), name='Direcciones_envio-detail'),
-    path('Categorias/', views.CategoriasListCreate.as_view(), name='Categorias-list'),
-    path('Categorias/<int:pk>/', views.CategoriasDetail.as_view(), name='Categorias-detail'),
-    path('Productos/', views.ProductosListCreate.as_view(), name='Productos-list'),
-    path('Productos/<int:pk>/', views.ProductosDetail.as_view(), name='Productos-detail'),
+
+    # Categorías
+    path('categorias/', views.CategoriasListCreate.as_view(), name='Categorias-list'),
+    path('categorias/<int:pk>/', views.CategoriasDetail.as_view(), name='Categorias-detail'),
+
+    # Productos
+    path('productos/', views.ProductosListCreate.as_view(), name='Productos-list'),
+    path('productos/<int:pk>/', views.ProductosDetail.as_view(), name='Productos-detail'),
+
+    # Carrito de compras
     path('CarritoDeCompras/', views.CarritoDeComprasListCreate.as_view(), name='CarritoDeCompras-list'),
     path('CarritoDeCompras/<int:pk>/', views.CarritoDeComprasDetail.as_view(), name='CarritoDeCompras-detail'),
+
+    # Órdenes
     path('Ordenes/', views.OrdenesListCreate.as_view(), name='Ordenes-list'),
     path('Ordenes/<int:pk>/', views.OrdenesDetail.as_view(), name='Ordenes-detail'),
-    path('Pagos/', views.PagosListCreate.as_view(), name='Pagos-list'),
-    path('Pagos/<int:pk>/', views.PagosDetail.as_view(), name='Pagos-detail'),
+
+    # Pagos
+    path('pagos/', views.PagosListCreate.as_view(), name='Pagos-list'),
+    path('pagos/<int:pk>/', views.PagosDetail.as_view(), name='Pagos-detail'),
+
+    # Autenticación y registro
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', views.RegistroView.as_view(), name='register'),
-    path('login/', views.LoginView.as_view(), name='login')
+    path('login/', views.LoginView.as_view(), name='login'),
 ]
--- BE_Creaciones_FayFa/api/views.py
+++ BE_Creaciones_FayFa/api/views.py
@@ -1,27 +1,21 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.contrib.auth.models import User
-from django.db.models import Count
-from .models import Usuarios, Direcciones_envio, Categorias, Productos, CarritoDeCompras, Ordenes, Pagos
-from .serializers import UsuariosSerializer, Direcciones_envioSerializer, CategoriasSerializer, ProductosSerializer, CarritoDeComprasSerializer, OrdenesSerializer, PagosSerializer, RegistroSerializer
-from rest_framework.views import APIView
+# from django.db.models import Count
+from .models import Direcciones_envio, Categorias, Productos, CarritoDeCompras, Ordenes, Pagos
+from .serializers import Direcciones_envioSerializer, CategoriasSerializer, ProductosSerializer, CarritoDeComprasSerializer, OrdenesSerializer, PagosSerializer, RegistroSerializer
+# from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.contrib.auth import authenticate, login
 from rest_framework_simplejwt.tokens import RefreshToken
 
 
-
-# Usuarios
-
-class UsuariosListCreate(generics.ListCreateAPIView):
-    queryset = Usuarios.objects.all()
-    serializer_class = UsuariosSerializer
+class RegistroView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegistroSerializer
     permission_classes = [AllowAny]
-    
-class UsuariosDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Usuarios.objects.all()
-    serializer_class = UsuariosSerializer
-    permission_classes = [AllowAny] 
+
+
     
 # Direcciones_envio
 
@@ -45,7 +39,7 @@ class CategoriasListCreate(generics.ListCreateAPIView):
 class CategoriasDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Categorias.objects.all()
     serializer_class = CategoriasSerializer
-    permission_classes = [AllowAny] 
+    permission_classes = [IsAuthenticated] 
 
 # Productos
 
@@ -57,7 +51,7 @@ class ProductosListCreate(generics.ListCreateAPIView):
 class ProductosDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Productos.objects.all()
     serializer_class = ProductosSerializer
-    permission_classes = [AllowAny] 
+    permission_classes = [IsAuthenticated] 
 
 # CarritoDeCompras
 
@@ -69,7 +63,7 @@ class CarritoDeComprasListCreate(generics.ListCreateAPIView):
 class CarritoDeComprasDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = CarritoDeCompras.objects.all()
     serializer_class = CarritoDeComprasSerializer
-    permission_classes = [AllowAny] 
+    permission_classes = [IsAuthenticated] 
     
 # Ordenes
 
@@ -81,10 +75,9 @@ class OrdenesListCreate(generics.ListCreateAPIView):
 class OrdenesDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Ordenes.objects.all()
     serializer_class = OrdenesSerializer
-    permission_classes = [AllowAny] 
+    permission_classes = [IsAuthenticated] 
     
 # Pagos
-
 class PagosListCreate(generics.ListCreateAPIView):
     queryset = Pagos.objects.all()
     serializer_class = PagosSerializer
@@ -93,12 +86,8 @@ class PagosListCreate(generics.ListCreateAPIView):
 class PagosDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Pagos.objects.all()
     serializer_class = PagosSerializer
-    permission_classes = [AllowAny] 
+    permission_classes = [IsAuthenticated] 
     
-class RegistroView(generics.CreateAPIView):
-    queryset = Usuarios.objects.all()
-    serializer_class = RegistroSerializer
-    permission_classes = [AllowAny]
     
 class LoginView(generics.GenericAPIView):
     permission_classes = [AllowAny]
