4c4eee769e92a4bf0481cc5a1bfe73c5da5d9d97
BryanAB506 <baragon@fwdcostarica.com>
2024-11-25 13:19:41 -0600

    avance de conexion

---
+++ FE_Creaciones_FayFa/src/services/GetAdmin.jsx
@@ -0,0 +1,27 @@
+async function getAdmin() {
+    try {
+        const response = await fetch('http://127.0.0.1:8000/api/register/', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Error fetching users');
+        }
+
+        const admin = await response.json();
+        console.log('Respuesta del servidor:', admin);
+
+
+
+
+        return admin;
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        throw error;
+    }
+}
+
+export default getAdmin ;
